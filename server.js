053953cf47042b241340e6a5326e8444d14362a1
Obamium68 <andrea.f.matera@gmail.com>
2023-02-22 17:29:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ class User {
 // We'll use a Map to keep track of connected clients
 const clients = new Map();
 var users = [];
-
+//LANERAE
 
 server.on('connection', (socket) => {
   // Generate a unique id for the client
