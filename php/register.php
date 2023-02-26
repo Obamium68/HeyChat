50915f0638989b27c72947f89ab995c892288956
20Gianluca04 <gianluca.causarano@outlook.it>
2023-02-26 19:08:40 +0100

    Cambio nome file css e prove register.php
    Da solo

--- php/register.php
+++ php/register.php
@@ -7,15 +7,15 @@ $surname = trim($_POST['surname']);
 $username = trim($_POST['username']);
 $password = hash('sha256', $_POST['password']);
 
-if (in_array($username, get_all_used_usernames($conn))) {       //If username is alrady used
-    header("Location: http://localhost/heychat/html/Index.html?");  //Redirect to error page
-    echo "{'err': '1'}";
-} else {
+/*if (in_array($username, get_all_used_usernames($conn))) {       //If username is alrady used
+    echo '{"err": 1}'; //Username already used
+} else {*/
     $insert = "INSERT INTO USERS(Username, Name, Surname, Pwd) VALUES ('$username','$name','$surname','$password')";
-    if ($conn->query($insert) === TRUE) {
-        echo "New record created successfully";
+    if ($conn->query($insert)) {
+        echo '{"err": 0}'; //OK
     } else {
-        header("Location: http://localhost/heychat/html/error_page.html");
+        echo $conn->error
+        echo '{"err": 2}';
     }
-}
+//}
 ?>
